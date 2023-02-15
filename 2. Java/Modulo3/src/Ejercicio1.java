6b467e99f5fd6e639074f718de8ee9a5c98b6cb6
ETTICUR <ayecuretti@gmail.com>
2023-02-15 14:25:03 -0300

    ejercicio 1 resuelto

---
+++ 2. Java/Modulo3/src/Ejercicio1.java
@@ -0,0 +1,8 @@
+public class Ejercicio1 {
+
+   // String apellido = Gomez; // Le faltan comillas para ser un string
+    //int edad = "35"; // le sobran las comillas para ser un numero
+    //boolean = "false";  // falta el nombre de la variable
+    //String sueldo = 45857.90 // debería ser una cadena de texto
+    //double nombre = "Julián"; // debería ser un número
+}
