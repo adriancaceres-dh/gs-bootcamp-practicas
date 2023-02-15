98f8b9d14e7df083aa9f6d520f48cced97ca7280
giseburgos13 <gisela.burgos@sancorsalud.com.ar>
2023-02-15 14:11:28 -0300

    ejercicios 1 y 2

---
+++ 2. Java/ejercicioclase1/untitled/src/ejercicio1.java
@@ -0,0 +1,8 @@
+public class ejercicio1 {
+    String apellido = "Gomez";
+    int edad = 35;
+    boolean variable = false;
+    double sueldo = 45857.90;
+    String nombre = "Julián";
+}
+
---
+++ 2. Java/ejercicioclase1/untitled/src/ejercicio2.java
@@ -0,0 +1,25 @@
+public class ejercicio2 {
+    String nombre;
+    int edad;
+    double sueldo;
+    String apellido;
+    String direccion;
+    boolean licencia_de_conducir;
+    double estatura_de_la_persona;
+    int cantidad_de_hijos;
+}
+
+
+
+
+//Actividad 2:
+
+
+        /*String nombre;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean licencia_de_conducir;
+        double estatura_de_la_persona;
+        int cantidad_de_hijos;*/
