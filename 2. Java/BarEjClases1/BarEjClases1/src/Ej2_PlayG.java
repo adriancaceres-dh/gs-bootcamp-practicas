64b3b773ecf92f237d66589cc986ef053dc428be
bkfa27 <bkfreites@gmail.com>
2023-02-15 12:32:15 -0300

    Add Ej2-PlayGround

---
+++ 2. Java/BarEjClases1/BarEjClases1/src/Ej2_PlayG.java
@@ -0,0 +1,18 @@
+public class Ej2_PlayG {
+    /* Un profesor de programación está corrigiendo los exámenes de sus estudiantes de la materia Programación
+     I para poder brindarles las correspondientes devoluciones. Uno de los puntos del examen consistía en
+     proponer nombres de variables, pero uno de los alumnos propuso nombres bastante peculiares.
+     ¿Podrías ayudar al profesor a detectar cuáles de estos nombres que utilizó el alumno no son nombres
+     de variables correctos?  Para ello, copiá la porción de código en tu IDE y validá los posibles errores.*/
+
+    String 1nombre; //Quitar los N° que no son válidos y dejar simplemente "nombre"
+    int edad;
+    double $ueldo; //Quitar el simbolo $ que no es válido y dejar "sueldo"
+    String @pellido; //Quitar el simbolo @ que no es válido y dejar "apellido"
+    String direccion;
+    boolean licencia_de_conducir;
+    double estatura de la persona; //Faltó el _ entre palabras para cumplir con el estilo de escritura
+    int cantidad-de-hijos; //Sustituir los - por _ entre palabras para cumplir con el estilo de escritura
+
+
+}
