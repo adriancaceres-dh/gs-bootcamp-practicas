26286f5a2032f0e1b67683d23c7b1fb21e8b08a8
NaconechneyN <naconechneynicolas@gmail.com>
2023-02-15 14:55:20 -0300

    ejercicios

---
+++ 2. Java/ejemplo1/untitled/src/ejercicio1.java
@@ -0,0 +1,17 @@
+public class ejercicio1 {
+    //String apellido = Gomez; le faltan las comillas a la asignacion
+    String apellido = "Gomez";
+
+    //int edad = "35"; El tipo de dato de la variable es un entero
+    int edad = 35;
+
+    //boolean = "false"; Falta el nombre de la variable y el valor no va entre comillas
+
+    boolean Nico = true;
+
+    //String sueldo = 45857.90 El tipo de dato está equivocado si se quiere mencionar un numero decimal, y falta el ";" para cerrar la declaracion
+    double sueldo = 45857.90;
+
+    //double nombre = "Julián"; El tipo de dato está equivocado si se quiere asignar un nombre  propio a la variable
+    String nombre = "Julian";
+}
---
+++ 2. Java/ejemplo1/untitled/src/ejercicio4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el monto bruto");
+
+        double monto = scanner.nextDouble();
+        double impuesto1 = monto * 2.45 / 100;
+        double impuesto2 = monto * 15 / 100;
+        double impuesto3 = monto * 3/ 100;
+
+        double totalImpuestos = impuesto1 + impuesto2 + impuesto3;
+
+        double premio = monto - totalImpuestos;
+        System.out.println("su premio es de $" + premio);
+
+    }
+}
