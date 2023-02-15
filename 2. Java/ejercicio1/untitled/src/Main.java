a8aafbf1431b2e99fdb1b5cc5af7db9986ce374e
marialauraflaxmarco <todosepuede_victoria@yahoo.com.ar>
2023-02-15 13:23:40 -0300

    primeros ejercicios java

--- 2. Java/ejercicio1/untitled/src/Main.java
+++ 2. Java/ejercicio1/untitled/src/Main.java
@@ -1,5 +1,71 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+/*
+        Ejercicio1
+        /*
+        String apellido = "Gomez"; // Le faltaron las comillas
+        int edad = 35; // sin comillas
+        boolean jueves = false; // sin comillas y nombre de la variable
+        double sueldo = 45857.90 ; // modificamos string a double y pusimos el punto y coma al final
+        String nombre = "Julián"; // modificamos double a string
+        */
+        /*
+        Ejercicio2
+        /
+        String 1nombre; // No puede empezar con un numero
+        int edad; //Correcto
+        double $ueldo; // No puede utilizar caracteres especiales
+        String @pellido; // No puede utilizar caracteres especiales
+        String direccion; //Correcto
+        boolean licencia_de_conducir;  //Correcto
+        double estatura de la persona; // No puede utilizar espacios
+        int cantidad-de-hijos; // No puede utilizar caracteres especiales
+        */
+
+        /*
+        Ejercicio 3
+        Scanner scanner = new Scanner(System.in);
+        int total;
+
+        System.out.println("Ingrese Cantidad de dias");
+
+        total = scanner.nextInt();
+
+        total = total*2;
+        System.out.println("El precio de la campaña es " + total);
+        */
+
+        /*
+        Ejercicio 4
+        Scanner scanner = new Scanner(System.in);
+        double impuesto1 = 2.45;
+        double impuesto2 = 15.00;
+        double impuesto3 = 3.00;
+        double total;
+        double impuestoTotal;
+        double totalNeto;
+
+        System.out.println("Ingrese el valor bruto del premio");
+
+        total = scanner.nextDouble();
+
+        impuestoTotal = total*(impuesto1+impuesto2+impuesto3)/100;
+
+        totalNeto = total - impuestoTotal;
+
+        DecimalFormat formatoDecimal = new DecimalFormat( "#.##");
+        String montoNetoPremioDecimal = formatoDecimal.format(totalNeto);
+
+        System.out.println("El monto neto del premio es " + montoNetoPremioDecimal);
+
+         */
+
+
+
+
+
     }
 }
+
+
