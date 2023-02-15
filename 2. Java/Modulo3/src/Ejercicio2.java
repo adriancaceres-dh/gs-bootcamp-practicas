68bba46a5c2a14164d4280a2ae83fb32addca826
ETTICUR <ayecuretti@gmail.com>
2023-02-15 14:30:13 -0300

    ejercicio 2

---
+++ 2. Java/Modulo3/src/Ejercicio2.java
@@ -0,0 +1,11 @@
+public class Ejercicio2 {
+   // String 1nombre; falta el nombre de la variable, el igual y las comillas
+   // int edad; falta el igual y el número entero
+   // double $ueldo; falta el igual y el número con decimal que es la variable
+    //String @pellido; falta el igual y la estructura de variable "variable", no se puede utilizar el arroba
+   // String direccion; falta el igual y el nombre de calle entre comillas
+   // boolean licencia_de_conducir; falta el true o false
+    //double estatura de la persona; falta el igual y el numero con getDecimal
+
+   // int cantidad-de-hijos; falta el igual y el numero entero
+}
