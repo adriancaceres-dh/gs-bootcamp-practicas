9d805c8be1669f86564a31834c01952b1f0bb0d6
ETTICUR <ayecuretti@gmail.com>
2023-02-15 14:03:20 -0300

    main y ejercicio3 resuelto

---
+++ 2. Java/Ejercicio1/src/Ejercicio4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Ingresé los días de campaña que va a realizar");
+        double precioCampania = scanner.nextDouble()*2;
+        System.out.println("El costo de su campaña en redes será " + precioCampania);
+    }
+}
