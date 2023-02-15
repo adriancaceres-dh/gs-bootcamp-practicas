d7ad7c354060a604b1ade2bb4a1d6e61ad1d9a1a
Gise-Loren <gisellorenzatti@gmail.com>
2023-02-15 12:09:11 -0300

    Ejercicio1

--- 2. Java/ejercicio1/Ejercicio1/src/Main.java
+++ 2. Java/ejercicio1/Ejercicio1/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int numero = scanner.nextInt();
+        System.out.println("El numero ingresado es " + numero);
+
+        System.out.print("Ingrese un numero ");
+        String nombre = scanner.next();
+        System.out.print("El nombre ingresado es " + nombre);
+
+       int i = 0;
+        System.out.println(i);
+        char letra = 'a';
+        System.out.println(letra);
+
+        String palabra = "Algo";
+        System.out.println(palabra);
+
+
     }
 }
