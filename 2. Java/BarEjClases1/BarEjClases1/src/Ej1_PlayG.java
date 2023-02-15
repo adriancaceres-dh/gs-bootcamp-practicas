cd0251c9b2b6d1c0f444fcba44ecd730049fd813
bkfa27 <bkfreites@gmail.com>
2023-02-15 12:19:48 -0300

    Add Ej1-PlayGround

---
+++ 2. Java/BarEjClases1/BarEjClases1/src/Ej1_PlayG.java
@@ -0,0 +1,13 @@
+public class Ej1_PlayG {
+
+    /*Un estudiante de programación intentó realizar declaraciones de variables con sus respectivos tipos
+    en Java pero tuvo varias dudas mientras lo hacía. A partir de esto, nos brindó su código y pidió la
+    ayuda de un desarrollador experimentado que pudiera darle una mano. ¿Podrías copiar su código en algún
+    IDE y verificar si es necesario realizar correcciones?  */
+
+    String apellido = Gomez; //Le faltaron las " " al valor, debería ser "Gomez"
+    int edad = "35"; //No lleva comillas
+    boolean = "false"; //Falta el nombre de la variable y quitarle las " " al valor false.
+    String sueldo = 45857.90 //Faltó el ; al final / el dato debe ser de texto y no numerico
+    double nombre = "Julián"; //El dato debe ser numerico y no de texto, este dato irís en String
+}
