7defc6cec9c65beea050392306558a60bd5e99b5
Gise-Loren <gisellorenzatti@gmail.com>
2023-02-15 12:24:59 -0300

    Ejercicio1

--- 2. Java/ejercicio1/Ejercicio1/src/Main.java
+++ 2. Java/ejercicio1/Ejercicio1/src/Main.java
@@ -8,17 +8,17 @@ public class Main {
         int numero = scanner.nextInt();
         System.out.println("El numero ingresado es " + numero);
 
-        System.out.print("Ingrese un numero ");
+        System.out.print("Ingrese un nombre ");
         String nombre = scanner.next();
         System.out.print("El nombre ingresado es " + nombre);
 
-       int i = 0;
+       /*int i = 0;
         System.out.println(i);
         char letra = 'a';
         System.out.println(letra);
 
         String palabra = "Algo";
-        System.out.println(palabra);
+        System.out.println(palabra);*/
 
 
     }
