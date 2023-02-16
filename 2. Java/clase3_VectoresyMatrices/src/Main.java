794a97dac416323438b108ab6527281eb855b10b
luchofesta <lucianoefesta@hotmail.com>
2023-02-16 13:58:23 -0300

    correccion actividad realizada antes de clases

--- 2. Java/clase3_VectoresyMatrices/src/Main.java
+++ 2. Java/clase3_VectoresyMatrices/src/Main.java
@@ -1,29 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-
         String[] ciudades = {"Londres","Madrid","Nueva York","Bs As", "Asuncion", "San Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"};
-       int [][] temperaturas = {{-2,-3,-8,4,6,5,0,-7,-1,-10}, {33,32,27,37,42,43,39,26,31,35}};
+        int [][] temperaturas = {{-2,33},{-3,32},{-8,27},{4,37},{6,42},{5,43},{0,39},{-7,26},{-1,31},{-10,35}};
 
-       int max = temperaturas[0][0];
-       int min = temperaturas[0][0];
-       int indiceMin = -1;
-        int indiceMax = -1;
+           int max = 0;
+           int min = 0;
+           int indiceMin = 0;
+           int indiceMax = 0;
 
-       for(int f = 0; f < temperaturas.length; f++){
-           for(int c = 0; c < temperaturas[f].length; c++){
-               if(temperaturas[f][c] < min){
-                   min = temperaturas[f][c];
-                   indiceMin = c;
-               }
-               if(temperaturas[f][c] > max){
-                   max = temperaturas[f][c];
-                   indiceMax = c;
+           for(int f = 0; f < temperaturas.length; f++){
+               for(int c = 0; c < temperaturas[f].length; c++){
+                   if(temperaturas[f][c] < min){
+                       min = temperaturas[f][c];
+                       indiceMin = f;
+                   }
+                   if(temperaturas[f][c] > max){
+                       max = temperaturas[f][c];
+                       indiceMax = f;
+                   }
                }
            }
-       }
-
-
-        System.out.println("El valor mínimo es: " + min + ". Registrada en la ciudad de: " + ciudades[indiceMin] + ".");
-        System.out.println("El valor máximo es: " + max + ". Registrada en la ciudad de: " + ciudades[indiceMax] + ".");
-    }
+            System.out.println("El valor mínimo es: " + min + ". Registrada en la ciudad de: " + ciudades[indiceMin] + ".");
+            System.out.println("El valor máximo es: " + max + ". Registrada en la ciudad de: " + ciudades[indiceMax] + ".");
+        }
 }
