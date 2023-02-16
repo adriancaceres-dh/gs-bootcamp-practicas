81d7a5c0f481f1d82f37df1d46fb1efa6a7a05cc
Mateo Ascona <asconamateo@gmail.com>
2023-02-16 11:50:02 -0300

    Ejercicio clase 2

---
+++ 2. Java/ejercicios/clase2/Ejercicios/src/Ejercicio2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese su DNI");
+        int dni = scanner.nextInt();
+        System.out.println("Ingrese su sueldo");
+        double sueldo = scanner.nextInt();
+
+        if(sueldo <= 20000){
+            System.out.println("A usted le corresponde un aumento de: 20%");
+            sueldo = sueldo * 1.20;
+            System.out.println("Su sueldo final sera: $" + sueldo);
+        } else if(sueldo > 20000 && sueldo <= 45000){
+            System.out.println("A usted le corresponde un aumento de: 10%");
+            sueldo = sueldo * 1.10;
+            System.out.println("Su sueldo final sera: $" + sueldo);
+        }else{
+            System.out.println("A usted le corresponde un aumento de: 5%");
+            sueldo = sueldo * 1.05;
+            System.out.println("Su sueldo final sera: $" + sueldo);
+        }
+
+    }
+}
