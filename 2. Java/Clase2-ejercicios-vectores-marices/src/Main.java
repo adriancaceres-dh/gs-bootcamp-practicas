fa127ad1bcda4f5ea401d9c53a5a9464c93595a6
Gise-Loren <gisellorenzatti@gmail.com>
2023-02-16 17:56:33 -0300

    cambios ejercicios

---
+++ 2. Java/Clase2-ejercicios-vectores-marices/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String ciudades [] = {"Londres", "Madrid", "New York", "Buenos Aires", "Assuncion", "Sao Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"};
+        int temperaturas [][] = {{-2, 33}, {-3,32}, {-8,27}, {4, 37}, {6,42}, {6,43}, {0,39}, {-7,26}, {-1,31}, {-10,35}};
+
+            int temperaturas =
+        System.out.println("Hello world!");
+    }
+
+    private static int nextInt() {
+    }
+}
