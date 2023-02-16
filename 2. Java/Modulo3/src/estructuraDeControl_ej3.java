39f507de63a89d84c29fa56f290fcd54ceb906bb
ETTICUR <ayecuretti@gmail.com>
2023-02-16 11:10:44 -0300

    estructura de control ejercicio 3 resuelto

---
+++ 2. Java/Modulo3/src/estructuraDeControl_ej3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class estructuraDeControl_ej3 {
+    public static void main(String[] args) {
+        Scanner scanner = new  Scanner(System.in);
+
+        int clientes =1;
+
+        while (clientes < 8) {
+            int clientesX = clientes++;
+            System.out.println("Elija un Servicio: 1 o 2");
+            int servicio = scanner.nextInt();
+
+            if (servicio == 1) {
+                System.out.println("El monto a pagar por el cliente " + clientesX + " es de $1500");
+            } else {
+                System.out.println("El monto a pagar por el cliente " + clientesX + " es de $2200");
+            }
+        }}}
