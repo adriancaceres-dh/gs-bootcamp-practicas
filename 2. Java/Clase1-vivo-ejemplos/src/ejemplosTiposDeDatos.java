81124d2778b96163f5d05793c95841249f22dd0a
Gise-Loren <gisellorenzatti@gmail.com>
2023-02-16 13:42:10 -0300

    Resolucion de ejercicios

---
+++ 2. Java/Clase1-vivo-ejemplos/src/ejemplosTiposDeDatos.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+public class ejemplosTiposDeDatos {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int numero = scanner.nextInt();
+        System.out.println("El numero ingresado es " + numero);
+
+
+        System.out.print("Ingrese un nombre ");
+        String nombre = scanner.next();
+        System.out.print("El nombre ingresado es " + nombre);
+
+
+	       /*int i = 0;
+	        System.out.println(i);
+	        short pequeno = 32767;
+	        byte masPequeno = 127;
+	        double numero = 10.0;
+	        float otroNumero =30.0F;
+	        System.out.println(numero + " " + masPequeno);
+	        boolean flag = true;
+
+	        char letra = 'a';
+	        System.out.println(letra);
+
+	        String palabra = "Algo";
+	        System.out.println(palabra);*/
+
+    }
+}
