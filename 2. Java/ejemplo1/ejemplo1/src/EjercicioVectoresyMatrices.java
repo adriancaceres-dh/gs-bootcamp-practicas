be713286a43bce8ad5953837fc8629176d9f863b
GiancarloGuitre <giancarlosalasia@hotmail.com>
2023-02-16 16:58:53 -0300

    ejercicios vectores y matrices

---
+++ 2. Java/ejemplo1/ejemplo1/src/EjercicioVectoresyMatrices.java
@@ -0,0 +1,27 @@
+public class EjercicioVectoresyMatrices {
+    public static void main(String[] args) {
+        String ciudades [] = {"Londres", "Madrid", "Nueva York", "Buenos Aires", "Asunción", "Sao Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"};
+        int temperaturas [] [] = {{-2,33},{-3,32},{-8,27},{4,37},{6,42},{6,43},{0,39},{-7,26},{-1,31},{-10,35}};
+
+        int temperaturaMaxima = 0;
+        int temperaturaMinima = 0;
+        int arrayFilaTemMin = 0;
+        int arrayFilaTemMax = 0;
+
+        for (int f=0; f<=9; f++) {
+            for (int c=0; c<=1; c++) {
+                if(temperaturaMaxima<temperaturas[f][c]){
+                    temperaturaMaxima = temperaturas[f][c];
+                    arrayFilaTemMax = f;
+                }
+                if(temperaturaMinima>temperaturas[f][c]){
+                    temperaturaMinima = temperaturas[f][c];
+                    arrayFilaTemMin = f;
+                }
+            }
+        }
+        System.out.println("La temperatura máxima registrada fue de "+ temperaturaMaxima+"°"+" en la ciudad de "+ ciudades[arrayFilaTemMax]);
+        System.out.println("La temperatura minima registrada fue de "+ temperaturaMinima+"°"+" en la ciudad de "+ ciudades[arrayFilaTemMin]);
+    }
+
+}
--- 2. Java/ejemplo1/ejemplo1/src/ejerciciosCondicionles3.java
+++ 2. Java/ejemplo1/ejemplo1/src/ejerciciosCondicionles3.java
@@ -27,4 +27,4 @@ public class ejerciciosCondicionles3 {
 
         }
     }
-}
+
