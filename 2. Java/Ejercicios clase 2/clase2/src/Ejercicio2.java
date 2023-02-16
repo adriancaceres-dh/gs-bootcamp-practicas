dc2ba60248e5819ff4971d38475c2c4407c956e4
jonatanjmg <Jonaldogarcia@gmail.com>
2023-02-16 11:52:17 -0300

    Ejercicios clase 2

---
+++ 2. Java/Ejercicios clase 2/clase2/src/Ejercicio2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese DNI: ");
+        int dni = scanner.nextInt();
+
+        System.out.println("Ingrese monto de sueldo: ");
+        double sueldo = scanner.nextDouble();
+
+        //Si el sueldo es menor o igual a $20.000 le corresponde un 20% de aumento.
+        //b. Si el sueldo es mayor a $20.000 pero menor o igual a $45.000 le corresponde un 10% de aumento.
+
+        double sueldoFinal = 0;
+
+        if (sueldo <= 20000){
+            sueldoFinal =  sueldo +(sueldo*0.20);
+        } else if (sueldo>20000 && sueldo <= 45000) {
+            sueldoFinal =  sueldo +(sueldo*0.10);
+        } else{
+            sueldoFinal =  sueldo +(sueldo*0.05);
+        }
+        System.out.println("El sueldo final es: " + sueldoFinal);
+    }
+
+}
---
+++ 2. Java/Ejercicios clase 2/clase2/src/Ejercicio3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int clientes = 1;
+
+        while (clientes < 8) {
+            int clientesX = clientes++;
+            System.out.println("Elija un Servicio: 1 o 2");
+            int servicio = scanner.nextInt();
+
+            if (servicio == 1) {
+                System.out.println("El monto a pagar por el cliente " + clientesX + " es de $1500");
+            } else {
+                System.out.println("El monto a pagar por el cliente " + clientesX + " es de $2200");
+            }
+        }
+    }
+}
+
