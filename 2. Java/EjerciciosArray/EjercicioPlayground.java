4323cbfe138d36fc2df99383326e9ce4aa1b13af
salvagauchat <salvagauchat@gmail.com>
2023-02-17 10:17:17 -0300

    correcion ejercicios array2

--- 2. Java/EjerciciosArray/EjercicioPlayground.java
+++ 2. Java/EjerciciosArray/EjercicioPlayground.java
@@ -5,8 +5,8 @@ public class EjercicioPlayground {
         String[]ciudades={"Londres","Madrid","Nueva Jork","Buenos Aires","Asuncion","Sao Pablo","Lima","Santiago de Chile","Lisboa","Tokio"};
         int[][]temperaturas={{-2,-3,-8,4,6,5,0,-7,-1,-10},{33,32,27,37,42,43,39,26,31,35}};
 
-        int mayor = temperaturas[1][0];
-        int menor = temperaturas[0][0];
+        int mayor = temperaturas[1][0]; // otra forma era poner Interger.MIN_VALUE arranca desde el menor valor de un entero
+        int menor = temperaturas[0][0]; // otra forma era poner Interger.MAX_VALUE arranca desde el mayor valor de un entero
         int posicionMayor=0;
         int posicionMenor=0;
 
