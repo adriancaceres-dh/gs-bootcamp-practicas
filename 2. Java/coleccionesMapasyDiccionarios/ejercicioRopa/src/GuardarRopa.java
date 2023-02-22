4dc3a64faf43713601fc4328e5cad422a6c8f331
FranCagnino <francagnino@gmail.com>
2023-02-22 08:44:24 -0300

    ejercicio integrador carrera selva

---
+++ 2. Java/coleccionesMapasyDiccionarios/ejercicioRopa/src/GuardarRopa.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class GuardarRopa {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> diccionario = new HashMap<>();
+        int contador = 0;
+
+        public Integer guardarPrendas
+        (List<Prenda>listadePrenda);
+    }
+    }
+
+    //ejercicio que no había que resolver
---
+++ 2. Java/coleccionesMapasyDiccionarios/ejercicioRopa/src/Prenda.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+public class Prenda {
+    public static void main(String[] args) {
+        String marca;
+        String modelo;
+    }
+
+    }
+
+}
+
+//ejercicio que no había que resolver
