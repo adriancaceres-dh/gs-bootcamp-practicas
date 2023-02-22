18a1de24aaf286bc13589ecdc1ad0ec517f62d34
giseburgos13 <gisela.burgos@sancorsalud.com.ar>
2023-02-22 08:22:05 -0300

    ejercicio de clase

---
+++ 2. Java/clase5_POO_clases_objetos_constructores/src/persona.java
@@ -0,0 +1,61 @@
+public class persona {
+
+    //ACTIVIDAD REALIZADA ANTES DE CLASES
+    //EJERCICIO 1
+    String nombre;
+    int edad;
+    String DNI;
+    double altura;
+    double peso;
+
+    //EJERCICIO 2
+    public Persona(){
+
+    }
+
+    public Persona(String nombre, int edad, String DNI){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.DNI = DNI;
+    }
+
+    public Persona(String nombre, int edad, String DNI, double altura, double peso){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.DNI = DNI;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    //EJERCICIO5
+    public double calcularIMC(){
+        double IMC = peso / (Math.pow(altura,2));
+
+        if(IMC < 20){
+            return -1;
+        }else if(IMC >= 20 && IMC <= 25){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
+    public boolean esMayordeEdad(){
+        if(edad >= 18){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", DNI='" + DNI + '\'' +
+                ", altura=" + altura +
+                ", peso=" + peso +
+                '}';
+    }
+}
