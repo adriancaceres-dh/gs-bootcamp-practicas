280bf38828cea663fa37c551355a0f212838be88
valentina-vallejos-podio <valentinavallejos2022@gmail.com>
2023-02-22 21:42:10 -0300

    ejercicios

---
+++ 2. Java/EJERCICIO 1/Ejercicio 1 Java/src/ExcepcionesyClasesUtilitarias/Calculos.java
@@ -0,0 +1,29 @@
+package ExcepcionesyClasesUtilitarias;
+
+
+    public class Calculos {
+
+        public static int obtenerMayor(int num1, int num2) {
+            return Math.max(num1, num2);
+        }
+
+        public static int obtenerMenor(int num1, int num2) {
+            return Math.min(num1, num2);
+        }
+
+        public static double calcularPotencia(double base, double exponente) {
+            return Math.pow(base, exponente);
+        }
+
+        public static double calcularCoseno(double angulo) {
+            return Math.cos(angulo);
+        }
+
+        public static double calcularRaizCuadrada(double numero) {
+            return Math.sqrt(numero);
+        }
+
+        public static int generarNumeroAleatorio() {
+            return (int) (Math.random() * 1000);
+        }
+    }
--- 2. Java/EJERCICIO 1/Ejercicio 1 Java/src/ExcepcionesyClasesUtilitarias/EjerciciosPlaygroundExcepcionesYClasesUtilitarias.java
+++ 2. Java/EJERCICIO 1/Ejercicio 1 Java/src/ExcepcionesyClasesUtilitarias/EjerciciosPlaygroundExcepcionesYClasesUtilitarias.java
@@ -1,8 +1,46 @@
 package ExcepcionesyClasesUtilitarias;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class EjerciciosPlaygroundExcepcionesYClasesUtilitarias {
+    /*
     public static void main(String[] args) {
-        PracticaExcepciones pe = new PracticaExcepciones();
-        pe.calcularCociente();
+        String mensajeFinal = "Este es el último mensaje";
+        int[] numeros = new int[5];
+        try {
+            numeros[5] = 10;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Se ha producido una excepción: " + e.getMessage());
+        }
+        System.out.println(mensajeFinal);
     }
+
+     */
+    public void leerArchivo(String nombreArchivo) {
+        String texto = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(nombreArchivo));
+            while ((texto = bufferedReader.readLine()) != null) {
+                System.out.println(texto);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("El archivo " + nombreArchivo + " no se ha encontrado.");
+        } catch (IOException e) {
+            System.err.println("Se ha producido un error de lectura del archivo " + nombreArchivo);
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (IOException e) {
+                System.err.println("Se ha producido un error al cerrar el archivo " + nombreArchivo);
+            }
+        }
+    }
+
 }
