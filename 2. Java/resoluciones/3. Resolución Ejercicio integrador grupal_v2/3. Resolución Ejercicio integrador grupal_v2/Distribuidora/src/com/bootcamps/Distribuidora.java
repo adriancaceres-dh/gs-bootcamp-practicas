dbe27da564e7b464519c4579198bf291d36c6e78
adriancaceres-dh <adrian.caceres@digitalhouse.com>
2023-02-23 15:27:26 -0300

    clases abstractas e interfaces en vivo

--- 2. Java/resoluciones/3. Resolución Ejercicio integrador grupal_v2/3. Resolución Ejercicio integrador grupal_v2/Distribuidora/src/com/bootcamps/Distribuidora.java
+++ 2. Java/resoluciones/3. Resolución Ejercicio integrador grupal_v2/3. Resolución Ejercicio integrador grupal_v2/Distribuidora/src/com/bootcamps/Distribuidora.java
@@ -13,13 +13,21 @@ public class Distribuidora {
         productos[2] = new NoPerecedero("fideos", 30, "tipo1");
 
         //Calculo el precio total
-        double total = 0;
-        for (Producto producto: productos) {
-            total += producto.calcular(5); //Polimorfismo
-        }
+        double total = calcularTotal(productos);
 
         //Muestro el total
         System.out.println("el total es "+total);
 
     }
+
+    private static double calcularTotal(Producto[] productos) {
+        double total = 0;
+        for (Producto producto: productos) {
+            total += producto.calcular(5); //Polimorfismo
+        }
+        return total ;
+
+    }
+
+
 }
