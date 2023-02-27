28394c45542b671c835f801b2a7ea5185a7e99f1
ETTICUR <ayecuretti@gmail.com>
2023-02-27 14:34:54 -0300

    mod 3 - lambda y Wrapper

---
+++ 2. Java/Mod3LambsaWrapperEj1/src/Garage.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Garage {
+    private int id;
+    private List<Vehiculo> listaVehiculos = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Vehiculo> getListaVehiculos() {
+        return listaVehiculos;
+    }
+
+    public void setListaVehiculos(List<Vehiculo> listaVehiculos) {
+        this.listaVehiculos = listaVehiculos;
+    }
+
+    public Garage() {
+    }
+
+    public Garage(int id, List<Vehiculo> listaVehiculos) {
+        this.id = id;
+        this.listaVehiculos = listaVehiculos;
+    }
+
+    @Override
+    public String toString() {
+        return "Garage{" +
+                "id=" + id +
+                ", listaVehiculos=" + listaVehiculos +
+                '}';
+    }
+}
