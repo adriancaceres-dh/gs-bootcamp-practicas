b0e8f77f35fb14883675bbc084bb3d04ca65f60a
PamelaAbeldanio <pbeldanio@gmail.com>
2023-02-27 18:42:15 -0300

    Numeros Romanos

---
+++ 3. Spring/Clase27-02/NumerosRomanos/numerosRomanos/src/main/java/com/bootcamp/numerosRomanos/NumerosRomanos.java
@@ -0,0 +1,34 @@
+package com.bootcamp.numerosRomanos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NumerosRomanos {
+    @GetMapping("{/number}")
+    public String romano(@PathVariable Integer numero) {
+        StringBuilder numeroRomano = new StringBuilder();
+        int[] numeroDecimal = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] numeroRoman = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        for (int i = 0; i < numeroDecimal.length; i++)
+            for (;numero >= numeroDecimal[i]; numero -= numeroDecimal[i])
+                numeroRomano.append(numeroRoman[i]);
+        return numeroRomano.toString();
+    }
+}
+
+
+/*
+* public String toRoman(@PathVariable Integer number) {
+    StringBuilder romanNumber = new StringBuilder();
+    int[] numbersToCompare = {1000,900,500,400,100,90,50,40,10, 9, 5, 4, 1};
+    String[] romanNumbers = {"M","CM","D","CD","C","XC","L","XL", "X", "IX", "V", "IV", "I"};
+
+    for (int i = 0; i < numbersToCompare.length; i++)
+      for (;number >= numbersToCompare[i]; number -= numbersToCompare[i])
+        romanNumber.append(romanNumbers[i]);
+
+    return romanNumber.toString();
+  }*/
