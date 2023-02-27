46be0fba2c01f34ddf5bc7bea49bd31a1b800779
NicoAtaides <nicoataides@gmail.com>
2023-02-27 16:58:14 -0300

    ejercicio1 spring

---
+++ 3. Spring/ejemplo-c1/src/main/java/com/bootcamp/ejemplo/Saludo.java
@@ -0,0 +1,27 @@
+package com.bootcamp.ejemplo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Saludo {
+    public String nombre;
+    public String apellido;
+
+    public  int edad;
+
+
+    @GetMapping("/bay/{nombre}/{apellido}/{edad}")
+    public String bay(@PathVariable String nombre,
+                      @PathVariable String apellido,
+                      @PathVariable int edad){
+        return "Tu nombre es " + nombre + ", tu apellido es " + apellido + " y tenes " + edad;
+    }
+
+
+
+
+}
+
