e7944e0dda6fbe792b68f0e1e9ec88119a136173
GiancarloGuitre <giancarlosalasia@hotmail.com>
2023-02-28 15:28:41 -0300

    ejercicio calculando edad

---
+++ 3. Spring/calculadoraDeEdades/src/main/java/com/bootcamp/calculadoraDeEdades/CalculadoraDeEdadesApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.calculadoraDeEdades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraDeEdadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraDeEdadesApplication.class, args);
+	}
+
+}
---
+++ 3. Spring/calculadoraDeEdades/src/test/java/com/bootcamp/calculadoraDeEdades/CalculadoraDeEdadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.calculadoraDeEdades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraDeEdadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
