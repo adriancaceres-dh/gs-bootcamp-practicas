44a0a6feecea267f14f292b70c86d61aea63702a
luchofesta <lucianoefesta@hotmail.com>
2023-02-28 15:42:09 -0300

    Modificacion luego de clases

--- 3. Spring/clase2_ejercicioCalculadora/src/main/java/com/example/Spring/clase2_ejercicioCalculadora/controller/CalculadoraEdad.java
+++ 3. Spring/clase2_ejercicioCalculadora/src/main/java/com/example/Spring/clase2_ejercicioCalculadora/controller/CalculadoraEdad.java
@@ -2,6 +2,7 @@ package com.example.Spring.clase2_ejercicioCalculadora.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -11,10 +12,21 @@ import java.time.Period;
 public class CalculadoraEdad {
     @GetMapping("/{dia}/{mes}/{anio}")
     public String edad(@PathVariable Integer dia, @PathVariable Integer mes, @PathVariable Integer anio){
+        return calcularFecha(dia,mes,anio);
+    }
+
+    @GetMapping("/obtenerfecha")
+
+    //aca pasamos la utl localhost:8080/obtenerfecha?dia=24&mes=10&anio=1992
+    public String obtenerfechaPorURL(@RequestParam Integer dia, @RequestParam Integer mes, @RequestParam Integer anio){
+        return calcularFecha(dia,mes,anio);
+    }
+
+    //Metodo para reutilizar codigo
+    private String calcularFecha(Integer dia, Integer mes, Integer anio){
         LocalDate fechaActual = LocalDate.now();
         LocalDate fechaIngresada = LocalDate.of(anio,mes,dia);
         Period diferencia = Period.between(fechaIngresada,fechaActual);
-        //System.out.println(diferencia);
 
         return "La edad de la persona es: " + diferencia.getYears() + " años."
                 + " Edad detallada: " + diferencia.getDays() + " días, " + diferencia.getMonths() + " Meses, " + diferencia.getYears() + " años." ;
