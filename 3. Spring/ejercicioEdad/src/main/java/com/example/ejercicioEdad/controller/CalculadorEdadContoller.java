3b2cacb50c10157ec7344e885e68e9d2f7eceaee
StefanoFabbro <stefano.fabbro@sancorsalud.com.ar>
2023-02-28 16:56:12 -0300

    Ejercicio edad sin errores

--- 3. Spring/ejercicioEdad/src/main/java/com/example/ejercicioEdad/controller/CalculadorEdadContoller.java
+++ 3. Spring/ejercicioEdad/src/main/java/com/example/ejercicioEdad/controller/CalculadorEdadContoller.java
@@ -2,30 +2,31 @@ package com.example.ejercicioEdad.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
-// *** EJERCICIO CON ERROR, NO LO PUDE DETECTAR ***
 
 
 @RestController
 public class CalculadorEdadContoller {
 
     // Forma 1
-    @GetMapping ("/{dia}{mes}{anio}")
+    @GetMapping ("/{dia}/{mes}/{anio}")
     public Integer obtenerEdad(@PathVariable Integer dia,
                                @PathVariable Integer mes,
                                @PathVariable Integer anio) {
         LocalDate fechaActual = LocalDate.now();
         LocalDate fechaNacimiento = LocalDate.of(anio,mes,dia);
         Long respuesta = ChronoUnit.YEARS.between(fechaNacimiento, fechaActual);
+        System.out.println("Programa corriendo");
         return respuesta.intValue();
     }
 
         // Forma 2
-    /*@GetMapping ("/obtenerfecha")
+    @GetMapping ("/obtenerfecha")
     public Integer obtenerEdadPorUrlGet (@RequestParam Integer dia,
                                 @RequestParam Integer mes,
                                 @RequestParam Integer anio) {
@@ -33,5 +34,5 @@ public class CalculadorEdadContoller {
         LocalDate fechaNacimiento = LocalDate.of(anio, mes, dia);
         Long respuesta = ChronoUnit.YEARS.between(fechaNacimiento, fechaActual);
         return respuesta.intValue();
-    }*/
+    }
 }
