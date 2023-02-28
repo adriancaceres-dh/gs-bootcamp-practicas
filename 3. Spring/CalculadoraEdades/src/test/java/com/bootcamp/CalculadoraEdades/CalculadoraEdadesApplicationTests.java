ec8ba0a83e458a2ffb659412976e250962250bec
MaineMartin22 <mmaine181@gmail.com>
2023-02-28 15:50:52 -0300

    ej edad

---
+++ 3. Spring/CalculadoraEdades/src/test/java/com/bootcamp/CalculadoraEdades/CalculadoraEdadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.CalculadoraEdades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraEdadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
