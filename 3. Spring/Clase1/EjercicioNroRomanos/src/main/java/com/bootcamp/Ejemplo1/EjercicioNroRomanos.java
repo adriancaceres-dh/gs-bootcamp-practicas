19ce587bf027fe6d9b0499f9f6ba3af197f4a641
salvagauchat <salvagauchat@gmail.com>
2023-02-28 10:32:20 -0300

    Ejercicio nro Romanos

---
+++ 3. Spring/Clase1/EjercicioNroRomanos/src/main/java/com/bootcamp/Ejemplo1/EjercicioNroRomanos.java
@@ -0,0 +1,13 @@
+package com.bootcamp.Ejemplo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioNroRomanos {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioNroRomanos.class, args);
+	}
+
+}
