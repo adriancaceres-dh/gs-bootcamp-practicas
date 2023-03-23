7b0eeeed1f2797e354fbfdd590170489cb5b0456
luchofesta <lucianoefesta@hotmail.com>
2023-03-23 10:14:01 -0300

    Opcion alt ultimo punto

--- 7. Bases de datos relacionales/Clase1_ActividadConsultasDB.sql
+++ 7. Bases de datos relacionales/Clase1_ActividadConsultasDB.sql
@@ -39,7 +39,12 @@ SELECT title, release_date FROM movies WHERE release_date BETWEEN "2004-01-01 00
 #Ordenar los resultados por rating.
 SELECT title, rating, awards, release_Date FROM movies 
 WHERE rating > 3 AND awards > 1 AND release_date BETWEEN "1988-01-01 00:00:00" AND "2009-12-31 23:59:59"
-ORDER BY rating DESC
+ORDER BY rating DESC;
 
+#Traer el título de las películas con el rating mayor a 3, con más de 1 premio y con fecha de lanzamiento entre el año 1988 al 2009. 
+#Ordenar los resultados por rating.
+SELECT title, rating, awards, release_Date FROM movies 
+WHERE rating > 3 AND awards > 1 AND YEAR(release_date) BETWEEN "1988" AND "2009"
+ORDER BY rating DESC
 
 
