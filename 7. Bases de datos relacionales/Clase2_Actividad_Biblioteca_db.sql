84f7463a4d4e3a3c1ba293df4d0567982ef5f2b1
luchofesta <lucianoefesta@hotmail.com>
2023-03-24 11:11:25 -0300

    Finalizacion act Biblioteca

--- 7. Bases de datos relacionales/Clase2_Actividad_Biblioteca_db.sql
+++ 7. Bases de datos relacionales/Clase2_Actividad_Biblioteca_db.sql
@@ -35,3 +35,39 @@ SELECT * FROM estudiante
 WHERE apellido LIKE "F%"; 
 
 #Listar los autores del libro “El Universo: Guía de viaje”. (Se debe listar solamente los nombres).
+SELECT a.nombre
+FROM autor a JOIN libro l JOIN libroautor la
+ON la.id_Autor = a.idautor AND la.id_Libro = l.idlibro
+WHERE l.titulo = "El Universo: Guía de viaje";
+
+#¿Qué libros se prestaron al lector “Filippo Galli”?
+SELECT l.titulo, p.fechaPrestamo, e.nombre, e.apellido
+FROM prestamo p JOIN libro l JOIN estudiante e
+ON p.id_Lector = e.idLector AND p.id_Libro = l.idlibro
+WHERE e.nombre = "Galli" AND e.apellido = "Filippo";
+
+#Listar el nombre del estudiante de menor edad.
+SELECT e.nombre, e.apellido, e.edad 
+FROM estudiante e
+WHERE e.edad < 18;
+
+#Listar nombres de los estudiantes a los que se prestaron libros de Base de Datos.
+SELECT e.nombre, l.titulo, l.area
+FROM prestamo p JOIN estudiante e JOIN libro l
+ON p.id_Lector = e.idLector AND p.id_Libro = l.idlibro
+WHERE l.area = "Base de Datos";
+
+#Listar los libros que pertenecen a la autora J.K. Rowling.
+SELECT l.titulo, a.nombre
+FROM libro l JOIN libroautor la JOIN autor a
+ON la.id_Autor = a.idautor AND la.id_Libro = l.idlibro
+WHERE a.nombre = "Agatha Christie";
+
+
+
+
+
+
+
+
+
