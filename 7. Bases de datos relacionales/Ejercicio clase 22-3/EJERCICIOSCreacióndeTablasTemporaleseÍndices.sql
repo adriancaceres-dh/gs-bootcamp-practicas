7ad877a854ccedc508c21ec00c8957832c2386c6
Valentina-Vallejos-Podio <valentinavallejos2022@gmail.com>
2023-03-24 21:10:15 -0300

    agregando ejercicios

---
+++ 7. Bases de datos relacionales/Ejercicio clase 22-3/EJERCICIOSCreacióndeTablasTemporaleseÍndices.sql
@@ -0,0 +1,23 @@
+-- Ejercicio 1
+-- Con la base de datos “movies”, se propone crear una tabla temporal llamada “TWD” y guardar en la misma los episodios de todas las temporadas de “The Walking Dead”.
+-- Realizar una consulta a la tabla temporal para ver los episodios de la primera temporada.
+
+CREATE TEMPORARY TABLE TWD
+SELECT se.title AS Serie, s.title AS Temporada, e.title AS Capítulo
+FROM episodes e JOIN seasons s JOIN series se
+ON se.id = s.serie_id AND e.season_id = s.id 
+WHERE se.title = "The Walking Dead";
+
+SELECT * FROM TWD;
+
+SELECT * FROM TWD
+WHERE Temporada = "Primer Temporada";
+
+-- Ejercicio 2
+-- En la base de datos “movies”, seleccionar una tabla donde crear un índice y luego chequear la creación del mismo. 
+-- Analizar por qué crearía un índice en la tabla indicada y con qué criterio se elige/n el/los campos.
+
+CREATE INDEX nombreApellido_autor
+ON actors(first_name,last_name);
+
+SELECT first_name, last_name FROM actors;
--- 7. Bases de datos relacionales/Ejercicio clase 22-3/Ejercicio1JoinySubconsultas.sql
+++ 7. Bases de datos relacionales/Ejercicio clase 22-3/Ejercicio1JoinySubconsultas.sql
@@ -6,14 +6,16 @@ FROM series s
 JOIN genres g ON s.genre_id = g.id;
 
 -- Mostrar el título de los episodios, el nombre y apellido de los actores que trabajan en cada uno de ellos.
-
+SELECT e.title AS Episodio, a.first_name AS "Nombre Actor", a.last_name AS "Apellido Actor"
+FROM episodes e JOIN actors a JOIN actor_episode ae
+ON ae.actor_id = a.id AND ae.episode_id = e.id;
 
 
 -- Mostrar el título de todas las series y el total de temporadas que tiene cada una de ellas.
 SELECT s.title, COUNT(*) AS total_seasons
 FROM series s
 JOIN seasons ss ON s.id = ss.serie_id
-GROUP BY s.id
+GROUP BY s.title;
 
 -- Mostrar el nombre de todos los géneros y la cantidad total de películas por cada uno, siempre que sea mayor o igual a 3.
 SELECT g.name, COUNT(*) AS total_movies
@@ -23,5 +25,9 @@ GROUP BY g.id
 HAVING total_movies >= 3;
 
 -- Mostrar sólo el nombre y apellido de los actores que trabajan en todas las películas de la guerra de las galaxias y que estos no se repitan.
-
+SELECT a.first_name, a.last_name
+FROM movies m JOIN actors a JOIN actor_movie am
+ON a.id = am.actor_id AND m.id = am.movie_id
+WHERE m.title LIKE "La guerra de las galaxias%"
+GROUP BY a.id;
 
