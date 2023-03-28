b7bafa446c2eb702da4e076264230b1ef5b6fe45
Luciano Festa <lucianoefesta@hotmail.com>
2023-03-28 07:41:57 -0300

    modificacion

--- 8. Implementacion de bases de datos/Clase1_EjercicioVivo_Demo/src/main/java/com/JPA/demo/repository/IPersonRepository.java
+++ 8. Implementacion de bases de datos/Clase1_EjercicioVivo_Demo/src/main/java/com/JPA/demo/repository/IPersonRepository.java
@@ -4,4 +4,6 @@ import com.JPA.demo.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IPersonRepository  extends JpaRepository<Person, Integer> {  //simepre extiende de JpaRepo y se pasa 2 argumentos, la entidad y el tipo de dato del ID.
+
+
 }
